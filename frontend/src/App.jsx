081e42da9c991317dc5bb2aa01cc973f974227db
NontasBak <nontasbak@gmail.com>
2025-02-15 01:00:41 +0200

    Integrate Google Maps API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <APIProvider apiKey={API_KEY}>
+            <Map
+                style={{ width: "100vw", height: "100vh" }}
+                defaultCenter={{ lat: 40.6401, lng: 22.9444 }}
+                defaultZoom={13}
+                gestureHandling={"greedy"}
+                disableDefaultUI={true}
+            />
+        </APIProvider>
+    );
 }
 
-export default App
+export default App;
