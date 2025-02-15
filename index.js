138d903750fbcf2a2e4ad9a3ee5038e0fda98471
lp64ace <akomikron@gmail.com>
2025-02-15 22:14:14 +0200

    Updated the parking spot calculation method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import parking from './lib/parking.js';
 
+/*
 const app = express();
 
 app.get('/', (req, res) => {
@@ -10,7 +11,14 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
 	console.log('Server running...');
 });
+*/
 
-parking.OpenStreetMapFetchRoads('146304012').then((data) => {
-	parking.GeographicDataToParkingSpaces(data, 1000);
+[146304012, 1833523007, 57554537].forEach((node) => {
+	parking.OpenStreetMapFetchRoads(node, 1000).then((road) => {
+		parking.OpenStreetNodeInfo(node).then((info) => {
+			console.log(parking.GeographicDataToParkingSpaces(road).length, "spots in", info[0].tags.name);
+		});
+	});
 });
+
+console.log(await parking.OpenStreetNodeInfo(1833523007));
