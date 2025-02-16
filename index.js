f8cfc7a5377e1d3a98c792ace6df98e5272ef1a4
lp64ace <akomikron@gmail.com>
2025-02-16 11:44:18 +0200

    Added sample sql connection

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ import parking from './lib/parking.js';
 
 import express from "express";
 import fetch from "node-fetch";
+import mysql from 'mysql2/promise';
 
 const prt = 9000;
 const app = express();
+
+const config = {
+	host: 'localhost',
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+};
+
+const connection = await mysql.createConnection(config);
+
+const [rows] = await connection.query('SHOW TABLES');
+rows.forEach(row => {
+	console.log(Object.values(row)[0]);
+});
 
 app.listen(prt, () => console.log(`Backend running on port ${prt}`));
