60e99b0ff8cb00b93b9f91cfa60fe5223f91d5b5
lp64ace <akomikron@gmail.com>
2025-02-16 11:35:57 +0200

    Cleared the backend for later use

--- index.js
+++ index.js
@@ -7,22 +7,4 @@ import fetch from "node-fetch";
 const prt = 9000;
 const app = express();
 
-app.get("/api/maps", async (req, res) => {
-	const { lat, lon, place } = req.query;
-
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?${new URLSearchParams({
-        key: process.env.VITE_GOOGLE_MAPS_API_KEY,
-        address: `${lat},${lon}`,
-    })}`;
-
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        console.error("Error fetching Google Maps API:", error);
-        res.status(500).json({ error: "Failed to fetch Google Maps data" });
-    }
-});
-
 app.listen(prt, () => console.log(`Backend running on port ${prt}`));
